1618d968cdb72e2cba24dc0e9f337405ec0b50a5
unknown <dl@01.ad.phfund.com.cn>
2016-11-05 18:32:23 +0800

    修改Oracle的sql配置文件

--- maven-parent/sql/t_course.sql
+++ maven-parent/sql/t_course.sql
@@ -1,4 +1,4 @@
-/*
+﻿/*
 Navicat Oracle Data Transfer
 Oracle Client Version : 11.1.0.7.0
 
@@ -18,11 +18,11 @@ Date: 2016-11-04 16:52:17
 -- ----------------------------
 -- Table structure for t_course
 -- ----------------------------
-DROP TABLE "SSPDATA"."t_course";
-CREATE TABLE "SSPDATA"."t_course" (
-"course_id" NUMBER(10) NOT NULL ,
-"name" VARCHAR2(32 BYTE) NOT NULL ,
-"credit" NUMBER(3,1) NOT NULL 
+
+CREATE TABLE SSPDATA.t_course (
+course_id NUMBER(10) NOT NULL ,
+name VARCHAR2(32 BYTE) NOT NULL ,
+credit NUMBER(3,1) NOT NULL 
 )
 LOGGING
 NOCOMPRESS
@@ -33,15 +33,15 @@ NOCACHE
 -- ----------------------------
 -- Records of t_course
 -- ----------------------------
-INSERT INTO "SSPDATA"."t_course" VALUES ('2016000', '英语', '4.50');
-INSERT INTO "SSPDATA"."t_course" VALUES ('2016001', '数学', '3');
-INSERT INTO "SSPDATA"."t_course" VALUES ('2016002', '语文', '3.50');
-INSERT INTO "SSPDATA"."t_course" VALUES ('2016003', '计算机', '1.50');
-INSERT INTO "SSPDATA"."t_course" VALUES ('2016004', 'Java', '2');
-INSERT INTO "SSPDATA"."t_course" VALUES ('2016005', 'Html', '4.50');
-INSERT INTO "SSPDATA"."t_course" VALUES ('2016006', 'BootStrap', '5');
-INSERT INTO "SSPDATA"."t_course" VALUES ('2016007', 'J2EE', '6.50');
-INSERT INTO "SSPDATA"."t_course" VALUES ('2016008', 'Mybatis', '2');
+INSERT INTO SSPDATA.t_course VALUES ('2016000', '英语', '4.50');
+INSERT INTO SSPDATA.t_course VALUES ('2016001', '数学', '3');
+INSERT INTO SSPDATA.t_course VALUES ('2016002', '语文', '3.50');
+INSERT INTO SSPDATA.t_course VALUES ('2016003', '计算机', '1.50');
+INSERT INTO SSPDATA.t_course VALUES ('2016004', 'Java', '2');
+INSERT INTO SSPDATA.t_course VALUES ('2016005', 'Html', '4.50');
+INSERT INTO SSPDATA.t_course VALUES ('2016006', 'BootStrap', '5');
+INSERT INTO SSPDATA.t_course VALUES ('2016007', 'J2EE', '6.50');
+INSERT INTO SSPDATA.t_course VALUES ('2016008', 'Mybatis', '2');
 
 -- ----------------------------
 -- Indexes structure for table t_course
@@ -50,11 +50,13 @@ INSERT INTO "SSPDATA"."t_course" VALUES ('2016008', 'Mybatis', '2');
 -- ----------------------------
 -- Checks structure for table t_course
 -- ----------------------------
-ALTER TABLE "SSPDATA"."t_course" ADD CHECK ("course_id" IS NOT NULL);
-ALTER TABLE "SSPDATA"."t_course" ADD CHECK ("name" IS NOT NULL);
-ALTER TABLE "SSPDATA"."t_course" ADD CHECK ("credit" IS NOT NULL);
+ALTER TABLE SSPDATA.t_course ADD CHECK (course_id IS NOT NULL);
+ALTER TABLE SSPDATA.t_course ADD CHECK (name IS NOT NULL);
+ALTER TABLE SSPDATA.t_course ADD CHECK (credit IS NOT NULL);
 
 -- ----------------------------
 -- Primary Key structure for table t_course
 -- ----------------------------
-ALTER TABLE "SSPDATA"."t_course" ADD PRIMARY KEY ("course_id");
+ALTER TABLE SSPDATA.t_course ADD PRIMARY KEY (course_id);
+
+select * from t_course
